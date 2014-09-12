4ec103c00e439bb0f69fb0552fa52f198a792348
Jesus Diaz <jesus.diaz5@upr.edu>
2014-09-12 12:00:38 -0400

    added a class for auxiliary functions

---
+++ SiProR/src/core/AuxiliaryFunctions.java
@@ -0,0 +1,9 @@
+package core;
+/* This class will contain some functions 
+ * that help with other functions. For example 
+ * functions that change decimal number to binary 
+ * strings. 
+ */
+public class AuxiliaryFunctions {
+	
+}
