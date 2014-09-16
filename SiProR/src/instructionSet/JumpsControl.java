377793b2267ff8fd2771b09270cf7791ae239ae1
Roberto E. Cruz Quesada <rcquesada@gmail.com>
2014-09-15 23:37:15 -0400

    Updated JumpsControl.java

--- SiProR/src/instructionSet/JumpsControl.java
+++ SiProR/src/instructionSet/JumpsControl.java
@@ -1,35 +1,39 @@
 package instructionSet;
 import core.CU;
 
-/* 
+/** 
  * This class will contain instructions that
  * handle "jumps" and control functions of the CPU.
+ * author Roberto E. Cruz Quesada
  */
 public class JumpsControl {
 
-	/*
+	/**
 	 * This method performs a jump using a register's content.
 	 * The PC is updated using the content of the register
-	 * in the parameter. 
+	 * in the parameter.
+	 * @param regNumber - the number of the register being used. 
 	 */
 	public static void jmpr(int regNumber){
 		CU.pc = CU.reg[regNumber];
 	}
 	
-	/*
+	/**
 	 * This method performs a jump using an address' content.
 	 * The PC is updated using the content of the address 
-	 * in the parameter. 
+	 * in the parameter.
+	 * @param memIndex - the number representation of the address being used. 
 	 */
 	public static void jmpa(int memIndex){
 		CU.pc = CU.mem[memIndex];
 	}
 	
-	/*
+	/**
 	 * This method utilizes the "cond" variable to
 	 * permit, or not, a jump using a register's content. 
 	 * If the "cond" variable is equal to zero, 
 	 * the "nop()" method is called.
+	 * @param regNumber - the number of the register being used.
 	 */
 	public static void jcr(int regNumber){
 		if(CU.cond != 0)
@@ -42,11 +46,12 @@ public class JumpsControl {
 		}
 	}
 	
-	/*
+	/**
 	 * This method utilizes the "cond" variable to
 	 * permit, or not, a jump using an address' content. 
 	 * If the "cond" variable is equal to zero, 
 	 * the "nop()" method is called.
+	 * @param memIndex - the number representation of the address being used.
 	 */
 	public static void jca(int memIndex){
 		if(CU.cond != 0)
@@ -59,10 +64,12 @@ public class JumpsControl {
 		}
 	}
 	
-	/*
+	/**
 	 * This method provides a loop wherein the PC will not move
 	 * to the next instruction until the value of the register's
 	 * content is zero.
+	 * @param regNumber - the number for the register who's content will whether the loop will end or not.
+	 * @param memIndex - the number representation of the address to be used.
 	 */
 	public static void loop(int regNumber, int memIndex){
 		CU.reg[regNumber] = CU.reg[regNumber-1]; // Decrease the value of the register's content by 1.
@@ -74,10 +81,12 @@ public class JumpsControl {
 		CU.pc = CU.mem[memIndex];				   // Once the value is zero, update the PC accordingly.
 	}
 	
-	/*
+	/**
 	 * This method checks if the first register (in the first parameter)
 	 * is greater than the second register (in the second parameter).
 	 * Returns false if it's not greater.
+	 * @param regNumber1 - the number representing the first register.
+	 * @param regNumber2 - the number representing the second register.
 	 */
 	public static void gr(int regNumber1, int regNumber2){
 		if(CU.reg[regNumber1] > CU.reg[regNumber2]) // If the value in register1 is greater than the value in register2...
@@ -90,10 +99,12 @@ public class JumpsControl {
 		}
 	}
 	
-	/*
+	/**
 	 * This method checks if the first register (in the first parameter)
 	 * is greater or equal to the second register (in the second parameter).
 	 * Returns false if both conditions are not true.
+	 * @param regNumber1 - the number representing the first register.
+	 * @param regNumber2 - the number representing the second register.
 	 */
 	public static void gre(int regNumber1, int regNumber2){
 		if(CU.reg[regNumber1] > CU.reg[regNumber2] | CU.reg[regNumber1] == CU.reg[regNumber2])
@@ -106,9 +117,11 @@ public class JumpsControl {
 		}
 	}
 	
-	/*
+	/**
 	 * This method checks if two registers are equal.
 	 * Returns false if they are not.
+	 * @param regNumber1 - the number representing the first register.
+	 * @param regNumber2 - the number representing the second register.
 	 */
 	public static void eq(int regNumber1, int regNumber2){
 		if(CU.reg[regNumber1] == (CU.reg[regNumber2]))  // If the value in register1 is the same as the value of register2...
@@ -121,14 +134,16 @@ public class JumpsControl {
 		}
 	}
 	
-	/* 
+	/** 
 	 * This method checks if two registers are not equal.
 	 * Returns false if they are equal.
+	 * @param regNumber1 - the number representing the first register.
+	 * @param regNumber2 - the number representing the second register.
 	 */
 	public static void neq(int regNumber1, int regNumber2){
-		if(CU.reg[regNumber1] != CU.reg[regNumber2])  // Call "eq" function (a bit of recursion so to speak).
+		if(CU.reg[regNumber1] != CU.reg[regNumber2])	// If the value in register1 is not the same as the value of register2...
 		{										
-			CU.cond = 1;						// If the "eq" function returns false, then "neq" is true.
+			CU.cond = 1;
 		}
 		else									// Otherwise...
 		{
@@ -136,7 +151,7 @@ public class JumpsControl {
 		}
 	}
 	
-	/*
+	/**
 	 * This method performs a "no operation" instruction,
 	 * essentially "doing nothing" except a CPU clock cycle or the such.
 	 */
@@ -147,7 +162,7 @@ public class JumpsControl {
 		{}
 	}
 	
-	/*
+	/**
 	 * This method stops execution.
 	 */
 	public static void stop(){
